ebd2a995da73b8e8ba2bd22dfeea9bae4086dbb7
AryanGh <mahdi.gholami875@gmail.com>
2023-08-01 02:44:20 +0330

    fixing icon issue

--- types/AddCartType.ts
+++ types/AddCartType.ts
@@ -1,11 +1,17 @@
 export type AddCartType = {
   displayName: string;
   mainId: string;
-  displayAssets: string;
+  displayAssets: DisplayAssestType;
   icon: string;
   quantity?: number | 1;
   price: PriceType;
 };
+
+type DisplayAssestType = [
+  {
+    background: string
+  }
+]
 
 type PriceType = {
   finalPrice: number;
--- types/ItemsType.ts
+++ types/ItemsType.ts
@@ -1,10 +1,16 @@
 export type ItemsType = {
   displayName: string;
   mainId: string;
-  displayAssets: string;
+  displayAssets: DisplayAssestType;
   quantity?: number | 1;
   price: PriceType;
 };
+
+type DisplayAssestType = [
+  {
+    background: string
+  }
+]
 
 type PriceType = {
   finalPrice: number;
